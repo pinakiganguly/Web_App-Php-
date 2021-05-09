f6a1c535eb311a5c84b3eca2ee9eee5b16d27d59
pinakiganguly <59434679+pinakiganguly@users.noreply.github.com>
2021-05-09 22:00:38 +0530

    Add files via upload

---
+++ crack.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<head><title>Pinaki Ganguly MD5</title></head>
+<body style="font-family: aerial">
+<h1>MD5 cracker</h1>
+<p>This application takes an MD5 hash
+of a four digit pin and check all 10,000
+possible four digit PINs to determine the PIN.</p>
+<pre>Debug Output:
+4a7d1ed414474e4033ac29ccb8653d9b 0000
+25bbdcd06c32d477f7fa1c3e4a91b032 0001
+fcd04e26e900e94b9ed6dd604fed2b64 0002
+7cd86ecb09aa48c6e620b340f6a74592 0003
+95b09698fda1f64af16708ffb859eab9 0004
+d39934ce111a864abf40391f3da9cdf5 0005
+7f8bb0fe8b33780a08fe6b60ced14529 0006
+6950aac2d7932e1f1a4c3cf6ada1316e 0007
+926abae84a4bd33c834bc6b981b8cf30 0008
+29549a71a57f587d88209b9c1f1b7999 0009
+fc1198178c3594bfdda3ca2996eb65cb 0010
+ae2bac2e4b4da805d01b2952d7e35ba4 0011
+29150bb2319c182c944841c74d2f8d75 0012
+c0279f73075a52e1a7dea35065bc8c80 0013
+b6fb522815d06fed82b0140be4c74680 0014
+Total checks: 10000
+Ellapsed time: 0.0047969818115234
+</pre>
+<p>PIN:Not Found</p>
+<form>
+<input type="text" name="md5" size="40" 
+  value="1ca906c1ad59db8f11643829560bab55"/>
+<input type="submit" value="Crack MD5"/>
+</form>
+<ul>
+<li><a href="index.php">Reset this page</a></li>
+<li><a href="makepin.php">Make an MD5 PIN</a></li>
+<li><a href="md5.php">MD5 Encoder</a></li>
+<li><a href="https://www.wa4e.com/assn/crack/" target="_blank">Specification
+for this assignment</li>
+<li><a 
+href="https://github.com/csev/php-intro/tree/master/code/crack" 
+target="_blank">Source code similar to this application</a></li>
+</ul>
+</body>
+</html>
+
