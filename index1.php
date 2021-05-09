6e3777770cfdec7348e52842690920c868b7b4bb
pinakiganguly <59434679+pinakiganguly@users.noreply.github.com>
2021-05-09 21:59:27 +0530

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,27 @@
+<html>
+    <title>
+    Pinaki Ganguly
+    </title>
+    <body>
+        <h1>Pinaki Ganguly PHP </h1>
+        <p>
+        The SHA256 hash of "Pinaki Ganguly" is
+            <?php
+                print hash('sha256','Milan Bauri');
+            ?>
+        </p>
+<pre>
+ASCII art:
+        *  * *
+        *      *
+        *     *
+        * *  *
+        *
+        *
+        *
+        </pre>
+        <a href="check.php">Click here to check the error setting</a>
+        <br>
+        <a href="fail.php">Click here to cause a traceback</a>
+    </body>
+</html>
