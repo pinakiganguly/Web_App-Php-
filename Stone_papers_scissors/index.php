106748d97e44d7e085f402e575467db20d48abf9
pinakiganguly <59434679+pinakiganguly@users.noreply.github.com>
2021-06-29 12:45:33 +0530

    Add files via upload

---
+++ Stone_papers_scissors/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>4c4d7d72</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Broken Rock Paper Scissors</h1>
+</p>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to 
+<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+<p>
+
+</p>
+</div>
+</body>
+
